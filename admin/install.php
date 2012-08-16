e32d5979eeedd3d5685f0ae526bfcde443901393
thiadmer.riemersma@gmail.com <thiadmer.riemersma@gmail.com@956d1ce0-6caf-7d0d-48d8-4a8382747a2a>
2012-08-16 12:57:25 +0000

    The language of each URL (page) is not also stored in the database. However, you must re-create the database (i.e. erase the existing database and then run install.php again). When a language is set for an URL (page) and this language is not the same as the user language (as passed through the form), the weight of the match is decreased. This way, if a page exists in multiple languages, the one in the user's language comes on top. The language of an URL is also passed to the search_results template page, so that you can adapt it to show the language of each link in the search results. The language is passed as $qry_results['lang'].

--- admin/install.php
+++ admin/install.php
@@ -7,12 +7,13 @@
 	</head>
 <body>
 <h2>Sphider installation script.</h2>
+<ul>
 <?
 error_reporting (E_ALL | E_STRICT);
 $settings_dir = "../settings";
 include "$settings_dir/database.php";
 
-echo "<p>sites\n";
+echo "<li>sites";
 $error = 0;
 $db->exec("create table ".$table_prefix."[sites](
 	site_id INTEGER PRIMARY KEY NOT NULL,
@@ -24,62 +25,69 @@ $db->exec("create table ".$table_prefix."[sites](
 	required TEXT,
 	disallowed TEXT,
 	can_leave_domain bool)");
+echo " ok\n";
 
-echo "<p>links\n";
+echo "<li>links";
 $db->exec("create table ".$table_prefix."[links] (
 	link_id INTEGER  PRIMARY KEY NOT NULL,
 	site_id INTEGER,
 	url VARCHAR(255) NOT NULL UNIQUE,
 	title VARCHAR(200),
 	description VARCHAR(255),
+	language VARCHAR(2),
 	fulltxt TEXT,
 	indexdate date,
 	size FLOAT(2),
 	md5sum VARCHAR(32) UNIQUE,
 	visible INTEGER DEFAULT 0,
 	level INTEGER)");
+echo " ok\n";
 
-echo "<p>keywords\n";
+echo "<li>keywords";
 $db->exec("create table ".$table_prefix."[keywords] (
 	keyword_id INTEGER PRIMARY KEY NOT NULL,
 	keyword VARCHAR(30) NOT NULL UNIQUE,
 	metaphone1 VARCHAR(4),
 	metaphone2 VARCHAR(4)
 	)");
-
+echo " ok\n";
 
 for ($i=0;$i<=15; $i++) {
 	$char = dechex($i);
-	echo "<p>link_keyword$char\n";
+	echo "<li>link_keyword$char";
 	$db->exec("create table ".$table_prefix."link_keyword$char (
 		link_id INTEGER KEY NOT NULL,
 		keyword_id INTEGER KEY NOT NULL,
 		weight INTEGER(3),
 		domain INTEGER(4)
 		)");
+    echo " ok\n";
 }
 
-echo "<p>categories\n";
+echo "<li>categories";
 $db->exec("create table ".$table_prefix."categories (
 	category_id INTEGER PRIMARY KEY NOT NULL,
 	category TEXT,
 	parent_num INTEGER
 	)");
+echo " ok\n";
 
-echo "<p>site_category\n";
+echo "<li>site_category";
 $db->exec("create table ".$table_prefix."site_category (
 	site_id INTEGER,
 	category_id INTEGER
 	)");
+echo " ok\n";
 
-echo "<p>temp\n";
+echo "<li>temp";
 $db->exec("create table ".$table_prefix."temp (
 	link VARCHAR(255),
 	level INTEGER,
 	id VARCHAR (32)
 	)");
+echo " ok\n";
 
-echo "<p>pending\n";
+echo "<li>pending";
 $db->exec("create table ".$table_prefix."pending (
 	site_id INTEGER,
 	temp_id VARCHAR(32),
@@ -87,21 +95,26 @@ $db->exec("create table ".$table_prefix."pending (
 	count INTEGER,
 	num INTEGER
 )");
+echo " ok\n";
 
-echo "<p>query_log\n";
+echo "<li>query_log";
 $db->exec("create table ".$table_prefix."query_log (
 	query VARCHAR(255),
 	time timestamp(14),
 	elapsed FLOAT(2),
 	results INTEGER
 	)");
+echo " ok\n";
 
-echo "<p>domains\n";
+echo "<li>domains";
 $db->exec("create table ".$table_prefix."domains (
 	domain_id INTEGER  PRIMARY KEY NOT NULL,
 	domain VARCHAR(255))");
+echo " ok\n";
+?>
+</ul>
 
-
+<?
 if (@$error >0) {
 	print "<b>Creating tables failed. Consult the above error messages.</b>";
 } else {
