7bc843d183cf154d8e4a00a6c16db12306fc5ce2
Thiadmer Riemersma <thiadmer.riemersma@gmail.com>
2016-12-23 21:59:39 +0100

    Added Frisian language.

---
+++ languages/fy-language.php
@@ -0,0 +1,30 @@
+<?php
+$sph_messages =  Array (
+	"Categories" => "Kategoryen",
+	"CATEGORIES" => "KATEGORYEN",
+	"Untitled" => "Dokumint sûnder titel",
+	"Powered by" => "Powered by",
+	"Previous" => "Eardere",
+	"Next" => "Fierdere",
+	"Result page" => "Resultaten side",
+	"Only in category" => "Allinne yn kategory",
+	"Search" => "Sykje",
+	"All sites" => "Alle siden",
+	"Web pages" => "Web siden",
+	"noMatch" => "Gjin resultaten fûn.",
+	"ignoredWords" => "Dizze wurden binne oerslein (te koart of te algemien): %ignored_words",
+	"resultsFor" => "Resultaten foar:",
+	"Results" => "Lit resultaten sjen %from - %to fan %all %matchword (%secs sekonden)", //
+	"match" => "side",
+	"matches" => "siden",
+	"andSearch" => "EN",
+	"orSearch" => "OF",
+	"phraseSearch" => "Indentike sin",
+	"show" => "Lit sjen",
+	"resultsPerPage" => "Resultaten op in side",
+	"DidYouMean" => "Mooglik bedoelden jo",
+	"SearchPrompt" => "Sykterm"
+);
+?>
+
+
